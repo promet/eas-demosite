3b17849ca7b9763f59982a606c2673f33b49e106
Michelle Krejci <michelle@promethost>
2013-09-25 22:17:23 -0500

    Added new build process to build per environment.

---
+++ build/prod/build-prod.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+TMP_PWD=$PWD
+
+pushd $PWD
+
+# Pass all arguments to drush
+while [ $# -gt 0 ]; do
+  drush_flags="$drush_flags $1"
+  shift
+done
+
+drush="drush $drush_flags"
+build_path=$(dirname "$0")
+
+echo "Disabling modules we do not need on prod.";
+$drush dis $(cat $build_path/prod_mods_purge | tr '\n' ' ') -y
+echo "Uninstalling modules we do not need on prod.";
+$drush pm-uninstall $(cat $build_path/dev_mods_purge | tr '\n' ' ') -y
+echo "Enabling css and js caching.";
+$drush vset -y preprocess_css 1 &&
+$drush vset -y preprocess_js 1
+
+popd
